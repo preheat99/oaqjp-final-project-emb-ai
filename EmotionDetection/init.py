9136279f163f9247825904a2361228bfd058c624
preheat99 <prabhatamola99@gmail.com>
2025-04-19 12:48:28 +0530

    Change test_text

--- EmotionDetection/init.py
+++ EmotionDetection/init.py
@@ -2,6 +2,6 @@ from emotion_detection import emotion_detector
 
 
 if __name__ == "__main__":
-    test_text = "I love this new technology"
+    test_text = "I hate working long hours."
     result = emotion_detector(test_text)
     print(result)
