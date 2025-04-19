b3fe08516df305ee034238a86cce471bfbcb15dd
preheat99 <prabhatamola99@gmail.com>
2025-04-19 12:10:28 +0530

    add emotion_detector

---
+++ emotion_detection.py
@@ -0,0 +1,10 @@
+import requests, json # Import the requests library to handle HTTP requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } } # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
+    return response.text
+
+print(emotion_detector('Hello'))
