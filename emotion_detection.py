723f65cb374724bcc6b6335015a446fd5c7a267f
preheat99 <prabhatamola99@gmail.com>
2025-04-19 12:35:48 +0530

    print emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,6 +5,14 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } } # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
-    return response.text
+    return response.json()
 
-print(emotion_detector('Hello'))
+required_response = emotion_detector('I love this new technology')
+emotion_from_response = required_response['emotionPredictions'][0]['emotion']
+dominant_emotion = 'anger'
+for emotion in emotion_from_response.keys():
+    if emotion_from_response[emotion] > emotion_from_response[dominant_emotion]:
+        dominant_emotion = emotion
+
+emotion_from_response['dominant_emotion'] = dominant_emotion
+print(emotion_from_response)
