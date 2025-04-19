7e76825510993ef77bbd2aa3a9e424f58084c878
preheat99 <prabhatamola99@gmail.com>
2025-04-19 13:29:09 +0530

    Add error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,11 +6,22 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
     required_response = response.json()
-    emotion_from_response = required_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = 'anger'
-    for emotion in emotion_from_response.keys():
-        if emotion_from_response[emotion] > emotion_from_response[dominant_emotion]:
-            dominant_emotion = emotion
+    if response.status_code == 200:
+        emotion_from_response = required_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = 'anger'
+        for emotion in emotion_from_response.keys():
+            if emotion_from_response[emotion] > emotion_from_response[dominant_emotion]:
+                dominant_emotion = emotion
 
-    emotion_from_response['dominant_emotion'] = dominant_emotion
-    return emotion_from_response
+        emotion_from_response['dominant_emotion'] = dominant_emotion
+        return emotion_from_response
+    elif response.status_code == 400:
+        error_response = {
+                    'anger': None,
+                    'disgust': None,
+                    'fear': None,
+                    'joy': None,
+                    'sadness': None,
+                    'dominant_emotion': None
+        }
+        return error_response
